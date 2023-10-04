6da554bf661ae8205c9b8adb2e93b3f9cde9536b
Bitarovský Roman <bitaror@softec.sk>
2023-10-04 14:16:56 +0200

    spring init

---
+++ sipvs-be/src/main/java/fiit/stulib/sipvsbe/SipvsBeApplication.java
@@ -0,0 +1,13 @@
+package fiit.stulib.sipvsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipvsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipvsBeApplication.class, args);
+	}
+
+}
---
+++ sipvs-be/src/test/java/fiit/stulib/sipvsbe/SipvsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package fiit.stulib.sipvsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipvsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
